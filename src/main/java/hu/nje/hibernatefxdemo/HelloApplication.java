e797ccbc0ab5f12ea5e7ba86e51eb6feb84f3475
Daniel Badinszky <badinszky97@gmail.com>
2024-10-27 12:14:34 +0100

    sql átköltöztetve a controller osztályba

--- src/main/java/hu/nje/hibernatefxdemo/HelloApplication.java
+++ src/main/java/hu/nje/hibernatefxdemo/HelloApplication.java
@@ -13,63 +13,24 @@ import java.io.IOException;
 import java.util.List;
 
 public class HelloApplication extends Application {
-    static SessionFactory factory;
+
     HelloController controller;
-    List<Mertek> mertekek;
-    List<Megnevezes> megnevezesek;
-    List<Korlatozas> korlatozasok;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 768);
         controller = fxmlLoader.getController();
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        System.out.println("haha");
-        //try{
-            Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-            factory = cfg.buildSessionFactory();
-            Read();
-            factory.close();
-            controller.tableFeltolt(korlatozasok);
-        //}
-        //catch(Exception e){
-       //     System.out.println("EXCEPTION VAN: " + e);
-       // }
-
-
+        controller.Init();
     }
-    public void Read() {
-        System.out.println("Read()........");
-        Session session = factory.openSession();
-        Transaction t = session.beginTransaction();
-        System.out.println("--------------------------Mértékek");
-        mertekek = session.createQuery("from Mertek").list();
-        for(Mertek m : mertekek) {
-            System.out.println(m.getNev());
-        }
-        System.out.println("----------------------------Megnevezesek");
-        megnevezesek = session.createQuery("from Megnevezes").list();
-        for(Megnevezes m : megnevezesek) {
-            System.out.println(m.getNev());
-        }
 
-        System.out.println("----------------------------Korlatozasok");
-        korlatozasok = session.createQuery("from Korlatozas").list();
-        for(Korlatozas m : korlatozasok) {
-            System.out.print(m.getUtszam() + " ");
-            System.out.print(m.getTelepules() + " ");
-            System.out.print(m.getMegnevezes().getNev() + " ");
-            System.out.print(m.getMertek().getNev() + " ");
-            System.out.println();
-        }
 
-        t.commit();
-        session.close();
 
-    }
+
+
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/hu/nje/hibernatefxdemo/HelloController.java
+++ src/main/java/hu/nje/hibernatefxdemo/HelloController.java
@@ -5,10 +5,18 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
 public class HelloController {
+    static SessionFactory factory;
+    List<Mertek> mertekek;
+    List<Megnevezes> megnevezesek;
+    List<Korlatozas> korlatozasok;
     @FXML
     private Label welcomeText;
 
@@ -24,10 +32,68 @@ public class HelloController {
     @FXML private TableColumn<Korlatozas, String> mertekCol;
     @FXML private TableColumn<Korlatozas, Integer> sebessegCol;
 
+    public void Init(){
+        Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+        factory = cfg.buildSessionFactory();
+        Read();
+        //Hozzaad();
+        //factory.close();
+        tableFeltolt(korlatozasok);
+    }
+    public void Hozzaad(){
+        Korlatozas korlatozas = new Korlatozas();
+        korlatozas.setUtszam(1234);
+        korlatozas.setKezdet(1.0f);
+        korlatozas.setVeg(2.0f);
+        korlatozas.setTelepules("uj telepulesnev");
+        korlatozas.setMettol("2020");
+        korlatozas.setMeddig("2121");
+        korlatozas.setMegnevezes(megnevezesek.get(1));
+        korlatozas.setMertek(mertekek.get(1));
+        korlatozas.setSebesseg(66);
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+        session.save(korlatozas);
+        t.commit();
+        session.close();
+    }
+    public void Read() {
+        System.out.println("Read()........");
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+        System.out.println("--------------------------Mértékek");
+        mertekek = session.createQuery("from Mertek").list();
+        for(Mertek m : mertekek) {
+            System.out.println(m.getNev());
+        }
+        System.out.println("----------------------------Megnevezesek");
+        megnevezesek = session.createQuery("from Megnevezes").list();
+        for(Megnevezes m : megnevezesek) {
+            System.out.println(m.getNev());
+        }
+
+        System.out.println("----------------------------Korlatozasok");
+        korlatozasok = session.createQuery("from Korlatozas").list();
+        for(Korlatozas m : korlatozasok) {
+            System.out.print(m.getUtszam() + " ");
+            System.out.print(m.getTelepules() + " ");
+            System.out.print(m.getMegnevezes().getNev() + " ");
+            System.out.print(m.getMertek().getNev() + " ");
+            System.out.println();
+        }
+
+        t.commit();
+        session.close();
+
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
+        Integer szam = korlTable.getSelectionModel().getSelectedIndex();
+        System.out.println(korlatozasok.get(szam).getTelepules());
     }
 
     public void tableFeltolt(List<Korlatozas> lista){
